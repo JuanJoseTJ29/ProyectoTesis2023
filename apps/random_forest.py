b85aaa8f4d7190d7faa8aeadaf643222098b21dd
QPRodrigo <rodrigoqp0722@gmail.com>
2023-03-04 19:05:47 -0500

    'Combobox'

--- apps/random_forest.py
+++ apps/random_forest.py
@@ -1,7 +1,7 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sb
+#import matplotlib.pyplot as plt
+#import numpy as np
+#import pandas as pd
+#import seaborn as sb
 import time
 import datetime
 import streamlit as st
@@ -9,13 +9,21 @@ import streamlit as st
 def app():
     
     st.title('Prediccion de la Violencia Familiar') 
-
     st.subheader('Pregunta 01: Su esposo/compañero se pone (ponía) celoso o molesto si usted conversa (conversaba) con otro hombre')
+    pregunta_1 = st.radio("", ('SI','NO'))
     st.subheader('Pregunta 02: Su esposo/compañero la acusa (acusaba) frecuentemente de ser infiel')
+    pregunta_2 = st.radio("", ('SI','NO'))
     st.subheader('Pregunta 03: Su esposo/compañero le impide (impedia) que visite o la visiten sus amistades')
+    pregunta_3 = st.radio("", ('SI','NO'))
     st.subheader('Pregunta 04: Su esposo/compañero trata (trataba) de limitar las visitas/contactos a su familia')
+    pregunta_4 = st.radio("", ('SI','NO'))    
     st.subheader('Pregunta 05: Su esposo/compañero insiste (insistía) siempre en saber todos los lugares dónde usted va (iba)')
+    pregunta_5 = st.radio("", ('SI','NO'))    
     st.subheader('Pregunta 06: Su esposo/compañero desconfía (desconfiaba) de Ud. con el dinero')
+    pregunta_6 = st.radio("", ('SI','NO'))
     st.subheader('Pregunta 07: Su esposo/compañero alguna vez le ha dicho o le ha hecho cosas para humillarla delante de los demás')
+    pregunta_7 = st.radio("", ('SI','NO'))
     st.subheader('Pregunta 08: Su esposo/compañero la ha amenazado con hacerle daño a usted o a alguien cercano a usted')
+    pregunta_8 = st.radio("", ('SI','NO'))    
     st.subheader('Pregunta 09: Su esposo/compañero la ha amenzado con irse de casa, quitarle a las hijas e hijos o la ayuda económica')
+    pregunta_9 = st.radio("", ('SI','NO'))
